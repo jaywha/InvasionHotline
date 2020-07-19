79fa47897b783856b1b84db32cfad5bd56312b3b
Jay Whaley <jayw685@gmail.com>
2020-07-19 00:05:36 -0500

    Intermediary commit

--- InvasionHotline/MainWindow.xaml.cs
+++ InvasionHotline/MainWindow.xaml.cs
@@ -4,7 +4,11 @@ using Newtonsoft.Json.Linq;
 using RestSharp;
 using System;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
 using System.Windows.Threading;
 
 namespace InvasionHotline
@@ -12,13 +16,83 @@ namespace InvasionHotline
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        #region PropertyChange Impl
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propName = "")
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        #endregion
+
         public DispatcherTimer InvasionPoller = new DispatcherTimer();
         public DispatcherTimer DistrictPoller = new DispatcherTimer();
-        public ObservableCollection<Invasion> Invasions { get; set; }  = new ObservableCollection<Invasion>();
 
-        public ObservableCollection<District> Districts { get; set; } = new ObservableCollection<District>();
+        private ObservableCollection<Invasion> _invasions = new ObservableCollection<Invasion>();
+        public ObservableCollection<Invasion> Invasions {
+            get => _invasions;
+            set {
+                _invasions = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<District> _districts = new ObservableCollection<District>();
+        public ObservableCollection<District> Districts {
+            get => _districts;
+            set {
+                _districts = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<string> Bossbots { get; set; } = new ObservableCollection<string>() {
+            "Flunky",
+            "Pencil Pusher",
+            "Yesman",
+            "Micromanager",
+            "Micromanager",
+            "Downsizer",
+            "Head Hunter",
+            "Corporate Raider",
+            "The Big Cheese"
+        };
+
+        public ObservableCollection<string> Lawbots { get; set; } = new ObservableCollection<string>()
+        {
+            "Bottom Feeder",
+            "Bloodsucker",
+            "Bloodsucker",
+            "Double Talker",
+            "Ambulance Chaser",
+            "Back Stabber",
+            "Spin Doctor",
+            "Legal Eagle",
+            "Big Wig"
+        };
+
+        public ObservableCollection<string> Cashbots { get; set; } = new ObservableCollection<string>()
+        {
+            "Short Change",
+            "Penny Pincher",
+            "Tightwad",
+            "Bean Counter",
+            "Number Cruncher",
+            "Money Bags",
+            "Loan Shark",
+            "Robber Baron"
+        };
+
+        public ObservableCollection<string> Sellbots { get; set; } = new ObservableCollection<string>()
+        {
+            "Cold Caller",
+            "Telemarketer",
+            "Name Dropper",
+            "Glad Hander",
+            "Mover & Shaker",
+            "Two-Face",
+            "The Mingler",
+            "Mr. Hollywood"
+        };
 
         public MainWindow()
         {
@@ -42,8 +116,8 @@ namespace InvasionHotline
 
             var jsonResult = JsonConvert.DeserializeObject<DistrictRequest>(response.Content);
             if (jsonResult == null || string.IsNullOrWhiteSpace(jsonResult.ToString())) return;
-            Console.WriteLine(jsonResult.ToString());
-            foreach(var apiDistrct in jsonResult.populationByDistrict)
+            // Console.WriteLine(jsonResult.ToString());
+            foreach (var apiDistrct in jsonResult.populationByDistrict)
             {
                 var newDistrict = new District()
                 {
@@ -64,18 +138,19 @@ namespace InvasionHotline
 
             var jsonResult = JsonConvert.DeserializeObject<ToonHQInvasionRequest>(response.Content);
             if (jsonResult == null || jsonResult.invasions == null) return;
-            Console.WriteLine(jsonResult.ToString());
-            foreach(JObject apiInvasion in jsonResult.invasions)
+            // Console.WriteLine(jsonResult.ToString());
+            foreach (JObject apiInvasion in jsonResult.invasions)
             {
                 var invData = JsonConvert.DeserializeObject<ToonHQInvasionData>(apiInvasion.ToString());
                 var maxNumberOfCogs = Convert.ToDouble(invData.total);
-                var tickTime = new TimeSpan(0, 0, (int) ((invData.total - invData.defeated) / invData.defeat_rate));
+                var tickTime = new TimeSpan(0, 0, (int)((invData.total - invData.defeated) / invData.defeat_rate));
                 var timeStarted = new DateTime(invData.start_time).ToLocalTime();
                 var endTime = timeStarted.Add(tickTime);
                 var durationTime = DateTime.Now.Subtract(endTime);
 
 
-                var newInvasion = new Invasion() {
+                var newInvasion = new Invasion()
+                {
                     District = invData.district,
                     Cog = invData.cog,
                     Ticks = durationTime.Ticks,
@@ -89,19 +164,20 @@ namespace InvasionHotline
 
         private PackIconKind DetermineLogoBasedOn(string cogName)
         {
-            Console.WriteLine(cogName);
-            switch(cogName.Trim())
+            // Console.WriteLine(cogName);
+            switch (cogName.Trim())
             {
-                // All Cashbots
-                case "Short Change":
-                case "Penny Pincher":
-                case "Tightwad":
-                case "Bean Counter":
-                case "Number Cruncher":
-                case "Money Bags":
-                case "Loan Shark":
-                case "Robber Baron":
-                    return PackIconKind.CashUsdOutline;
+                // All Bossbots
+                case "Flunky":
+                case "Pencil Pusher":
+                case "Yesman":
+                case "Micromanager":
+                case "Micromanager":
+                case "Downsizer":
+                case "Head Hunter":
+                case "Corporate Raider":
+                case "The Big Cheese":
+                    return PackIconKind.Tie;
                 // All Lawbots
                 case "Bottom Feeder":
                 case "Bloodsucker":
@@ -113,16 +189,16 @@ namespace InvasionHotline
                 case "Legal Eagle":
                 case "Big Wig":
                     return PackIconKind.Gavel;
-                // All Bossbots
-                case "Flunky":
-                case "Pencil Pusher":
-                case "Yesman":
-                case "Micromanager":
-                case "Downsizer":
-                case "Head Hunter":
-                case "Corporate Raider":
-                case "The Big Cheese":
-                    return PackIconKind.Tie;
+                // All Cashbots
+                case "Short Change":
+                case "Penny Pincher":
+                case "Tightwad":
+                case "Bean Counter":
+                case "Number Cruncher":
+                case "Money Bags":
+                case "Loan Shark":
+                case "Robber Baron":
+                    return PackIconKind.CashUsdOutline;
                 // All Sellbots
                 case "Cold Caller":
                 case "Telemarketer":
@@ -136,6 +212,82 @@ namespace InvasionHotline
                 default:
                     return PackIconKind.Gear;
             }
+        }
+
+        private void grdspltColumns_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            colInvasions.Width = new GridLength(50, GridUnitType.Star);
+            colDistricts.Width = new GridLength(50, GridUnitType.Star);
+        }
+
+        private bool ToggleFilter = false;
+        private void btnFilter_Click(object sender, RoutedEventArgs e)
+        {
+            CollectionViewSource filterSource = new CollectionViewSource() { 
+                IsLiveFilteringRequested = true
+            };
+            filterSource.Source = Invasions;            
+
+            Console.WriteLine("=== Filter Options ===");
+
+            Console.WriteLine("Bossbots {\n");
+            foreach (string value in stkBossbots.SelectedItems)
+            {
+                Console.WriteLine("\t"+value);
+            }
+            Console.WriteLine("}\n");
+
+            Console.WriteLine("Lawbots {\n");
+            foreach (string value in stkLawbots.SelectedItems)
+            {
+                Console.WriteLine("\t" + value);
+            }
+            Console.WriteLine("}\n");
+
+            Console.WriteLine("Cashbots {\n");
+            foreach (string value in stkCashbots.SelectedItems)
+            {
+                Console.WriteLine("\t" + value);
+            }
+            Console.WriteLine("}\n");
+
+            Console.WriteLine("Sellbots {\n");
+            foreach (string value in stkSellbots.SelectedItems)
+            {
+                Console.WriteLine("\t" + value);
+            }
+            Console.WriteLine("}\n");
+
+            Console.WriteLine("=====================");
+
+            filterSource.Filter += (object filterSender, FilterEventArgs filterArgs) => {
+                Invasion invasion = filterArgs.Item as Invasion;
+                string cogName = invasion.Cog;
+
+                if (stkBossbots.SelectedItems.Count + stkLawbots.SelectedItems.Count + stkCashbots.SelectedItems.Count + stkSellbots.SelectedItems.Count == 0)
+                {
+                    filterArgs.Accepted = true;
+                    return;
+                }
+
+                if(stkBossbots.SelectedItems.Contains(cogName) || stkLawbots.SelectedItems.Contains(cogName) || stkCashbots.SelectedItems.Contains(cogName) || stkSellbots.SelectedItems.Contains(cogName))
+                {
+                    filterArgs.Accepted = true;
+                } else
+                {
+                    filterArgs.Accepted = false;
+                }
+
+                ToggleFilter = !ToggleFilter;
+                if (ToggleFilter)
+                {
+                    btnFilterInvasions.IsPopupOpen = false;
+                    btnFilterInvasions.Foreground = Brushes.Green;
+                } else
+                {
+                    btnFilterInvasions.Foreground = Brushes.Black;
+                }
+            };
         }
     }
 }
---
+++ InvasionHotline/README.md
@@ -0,0 +1,2 @@
+﻿# Invasion Hotline
+
